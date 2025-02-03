d3855b7bd5ba0c87125f1427cb9ffa8187eb81bc
Juangr4 <jgarciaruiz2001@gmail.com>
2025-02-03 16:47:33 +0100

    Added Ollama

---
+++ lua/plugins/ia.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'github/copilot.vim',
+  },
+  { 'David-Kunz/gen.nvim', enabled = false },
+  {
+    'nomnivore/ollama.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+
+    -- All the user commands added by the plugin
+    cmd = { 'Ollama', 'OllamaModel', 'OllamaServe', 'OllamaServeStop' },
+
+    keys = {
+      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+      {
+        '<leader>coo',
+        ":<c-u>lua require('ollama').prompt()<cr>",
+        desc = 'ollama prompt',
+        mode = { 'n', 'v' },
+      },
+
+      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+      {
+        '<leader>coG',
+        ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+        desc = 'ollama Generate Code',
+        mode = { 'n', 'v' },
+      },
+    },
+
+    ---@type Ollama.Config
+    opts = {},
+  },
+}
