16cb21174871cac81fe895ad79fa0117bb623e24
Juangr4 <jgarciaruiz2001@gmail.com>
2025-02-05 16:57:29 +0100

    Big refactor
    Changed from telescope to snack.picker
    Removed dressing.nvim

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,33 +8,37 @@ return {
       'MunifTanjim/nui.nvim',
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
-      },
-      default_component_configs = {
-        git_status = {
-          symbols = {
-            -- Change type
-            added = '✚',
-            deleted = '✖',
-            modified = '',
-            renamed = '󰁕',
-            -- Status type
-            untracked = '',
-            ignored = '',
-            unstaged = '󰄱',
-            staged = '',
-            conflict = '',
+    config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = false,
+            hide_dotfiles = true,
+            hide_gitignored = false,
           },
         },
-      },
-    },
-    config = function()
+        window = {
+          width = 30,
+        },
+        default_component_configs = {
+          git_status = {
+            symbols = {
+              -- Change type
+              added = '✚', -- NOTE: you can set any of these to an empty string to not show them
+              deleted = '✖',
+              modified = '',
+              renamed = '󰁕',
+              -- Status type
+              untracked = '', --'',
+              ignored = '', --'',
+              unstaged = '', --'󰄱',
+              staged = '', --'',
+              conflict = '',
+            },
+            align = 'right',
+          },
+        },
+      }
       vim.keymap.set('n', '<leader>e', ':Neotree toggle position=left<CR>', { noremap = true, silent = true, desc = 'Open [E]xplorer Tree' })
     end,
   },
