ca187c8f45b821018f9cebb5fe0509579023e99e
Juangr4 <jgarciaruiz2001@gmail.com>
2025-02-12 00:27:08 +0100

    Changed format settings
    Added javascript eslint formatter
    Changed conform to be lazy

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,30 @@
 return {
   'stevearc/conform.nvim',
-  opts = {},
-  config = function()
-    require('conform').setup {
-      format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
-      formatters_by_ft = {
-        lua = { 'stylua', lsp_format = 'fallback' },
-        -- Conform will run multiple formatters sequentially
-        python = { 'isort', 'black' },
-        -- Conform will run the first available formatter
-        javascript = { 'eslint', 'prettierd', 'prettier', stop_after_first = true },
-      },
-    }
-  end,
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      '<leader>bf',
+      function()
+        require('conform').format { async = true }
+      end,
+      mode = { 'n', 'v' },
+      desc = '[B]uffer [F]ormat',
+    },
+  },
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      python = { 'isort', 'black' },
+      javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+    },
+    -- Set default options
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+  },
 }
