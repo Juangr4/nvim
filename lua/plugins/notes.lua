1a6963395e79e374137e8555daf69df70a88c037
Juangr4 <jgarciaruiz2001@gmail.com>
2025-03-24 01:32:21 +0100

    Added flash, noice and fix notes in windows

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -10,7 +10,13 @@ return {
     ft = 'tex',
     -- tag = "v2.15",
     init = function()
-      vim.g.vimtex_view_method = 'zathura'
+      -- Check if the platform is Windows
+      -- If so, set the viewer to System default PDF viewer
+      if vim.fn.has 'win32' == 1 then
+        vim.g.vimtex_view_method = 'general'
+      else
+        vim.g.vimtex_view_method = 'zathura'
+      end
       vim.g.vimtex_view_forward_search_on_start = false
     end,
   },
